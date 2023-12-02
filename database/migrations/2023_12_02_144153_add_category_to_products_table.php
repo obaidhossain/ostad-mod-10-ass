c4dbb615d011a6bec8fc90175e6d054cee59e557
Obaid <rahat.ahat2010@gmail.com>
2023-12-02 23:05:12 +0600

    Missing saved files

--- database/migrations/2023_12_02_144153_add_category_to_products_table.php
+++ database/migrations/2023_12_02_144153_add_category_to_products_table.php
@@ -4,25 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class AddCategoryToProductsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            $table->string('category', 50)->after('description');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            $table->dropColumn('category');
         });
     }
-};
+}
